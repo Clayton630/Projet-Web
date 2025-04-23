aace73d7d6dade3df6d30aa6a5665b0b37137915
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-04-23 11:39:59 +0200

    La BDD a été reliée à la page d'accueil
    J'ai fait un premier lien de la bdd de notre projet, c'est encore tres rudimentaire, on ne fait qu'afficher les utilisateurs enregistrés (il n'yen a pas pour l'instant), mais c'est fonctionnel

---
+++ Premier lien a la bdd (version 0.1)/base_donnees.sql
@@ -0,0 +1,65 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: Utilisateurs
+#------------------------------------------------------------
+
+CREATE TABLE Utilisateurs(
+        id_user      Int NOT NULL ,
+        mot_de_passe Varchar (50) NOT NULL ,
+        nom          Varchar (50) NOT NULL ,
+        email        Varchar (50) NOT NULL ,
+        isadmin      Bool NOT NULL
+	,CONSTRAINT Utilisateurs_PK PRIMARY KEY (id_user)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Categories
+#------------------------------------------------------------
+
+CREATE TABLE Categories(
+        id_cat Int NOT NULL ,
+        nom    Varchar (50) NOT NULL
+	,CONSTRAINT Categories_PK PRIMARY KEY (id_cat)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Etablissments
+#------------------------------------------------------------
+
+CREATE TABLE Etablissments(
+        id_etab      Int NOT NULL ,
+        nom          Varchar (50) NOT NULL ,
+        adrresse     Varchar (50) NOT NULL ,
+        lattitude    Float (10.8) ,
+        longitude    Float (10.8) ,
+        id_cat       Int NOT NULL
+	,CONSTRAINT Etablissments_PK PRIMARY KEY (id_etab)
+
+	,CONSTRAINT Etablissments_Categories_FK FOREIGN KEY (id_cat) REFERENCES Categories(id_cat)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Retours
+#------------------------------------------------------------
+
+CREATE TABLE Retours(
+        id_retour Varchar(50) NOT NULL ,
+        note            Int NOT NULL ,
+        commentaire     Varchar (150) NOT NULL ,
+        date            Date NOT NULL ,
+        id_user         Int NOT NULL ,
+        id_etab         Int NOT NULL
+	,CONSTRAINT Retours_PK PRIMARY KEY (id_retour)
+
+	,CONSTRAINT Retours_Utilisateurs_FK FOREIGN KEY (id_user) REFERENCES Utilisateurs(id_user)
+	,CONSTRAINT Retours_Etablissments0_FK FOREIGN KEY (id_etab) REFERENCES Etablissments(id_etab)
+)ENGINE=InnoDB;
+
+
