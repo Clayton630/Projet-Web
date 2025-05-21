0bd418ef55402670d46a296c42e4facbef87057b
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-05-21 10:18:34 +0200

    Delete Premier lien a la bdd (version 0.1) directory

--- Premier lien a la bdd (version 0.1)/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from .models import User
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def home():
-    users = User.query.all()
-    return render_template('home.html', users=users)
