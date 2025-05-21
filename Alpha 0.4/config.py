6d96e0d2da20f67b164403dab10cf0ba59384c58
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-05-21 10:18:02 +0200

    Delete Alpha 0.4 directory

--- Alpha 0.4/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/retoursdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'un-truc-ultra-secret-et-unique'
