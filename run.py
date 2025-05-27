6368ed40f3c5e963d60076927a7951a656ff9364
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-05-27 22:57:19 +0200

    Suppression du bloc temporaire pour migrer la bdd

--- run.py
+++ run.py
@@ -4,16 +4,5 @@ from app import create_app
 
 app = create_app()
 
-# ====== BLOC TEMPORAIRE POUR INITIALISER LA BASE DE DONNÉES SUR RENDER ======
-# Ce bloc lance automatiquement la migration (création des tables) au démarrage,
-# uniquement le temps d’initialiser la base sur Render (plan gratuit, pas de shell/job possible).
-# Dès que tout fonctionne, SUPPRIME ce bloc et repousse le fichier sur GitHub !
-
-from flask_migrate import upgrade
-
-with app.app_context():
-    upgrade()
-# ====== FIN DU BLOC TEMPORAIRE ======
-
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
