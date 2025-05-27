80bfdc0d2438486f381688067f03673aa829a92c
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-05-27 13:13:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+"""Configuration principale de l’application Flask."""
+
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    """Paramètres globaux adaptés pour la production Render."""
+
+    # La chaîne de connexion à la base de données sera lue dans les variables d'environnement
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
