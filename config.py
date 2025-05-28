b3e60905f50018dc43848bc5c9653e40664f5d4f
Clayton630 <93291298+Clayton630@users.noreply.github.com>
2025-05-28 17:11:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,13 @@
 class Config:
     """Paramètres globaux (adapter pour la production)."""
 
+    # Chaîne de connexion à la base de données MySQL.
+    # Format : mysql+pymysql://utilisateur:motdepasse@adresse:port/nomdelabase
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/retoursdb"
+
+    # Désactive la détection automatique de modification des objets (économie de ressources).
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Clé secrète Flask utilisée pour sécuriser les sessions, cookies, etc.
+    # !!! À personnaliser en production (doit être très longue, unique et secrète)
     SECRET_KEY = "un-truc-ultra-secret-et-unique"
